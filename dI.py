0f04dd0e4e77100f291e87cb8ef86751cf924e67
WeLeaveTo-night <flyingfooz@protonmail.com>
2024-12-01 08:14:43 -0400

    I

---
+++ dI.py
@@ -0,0 +1,22 @@
+from collections import Counter
+
+loc1, loc2 = [], []
+
+with open('dI-input.txt', 'r') as file:
+    for line in file:
+        l1, l2 = line.split() 
+        loc1.append(int(l1))        
+        loc2.append(int(l2))
+
+loc1.sort()
+loc2.sort()
+
+d = sum(abs(l1 - l2) for l1, l2 in zip(loc1, loc2))
+
+## PART 2
+counts = Counter(loc2)
+
+sim_score = 0
+for loc in loc1:
+    if loc in counts:
+        sim_score += counts[loc] * loc
