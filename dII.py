64f6b46c4b454189837f3bcac092cc40c1126c42
WeLeaveTo-night <flyingfooz@protonmail.com>
2024-12-02 12:35:10 -0400

    'II'

---
+++ dII.py
@@ -0,0 +1,26 @@
+levels = []
+
+with open('dII-input.txt', 'r') as file:
+    for line in file:
+        levels.append(list(map(int, line.split())))
+
+safe_lvls = 0 
+for l in levels:
+    sign = 0
+    if l[0] < l[1]:
+        sign = 1
+    elif l[0] > l[1]:
+        sign = -1
+    else: 
+        continue
+
+    safe = 0
+    if sign == 1:
+        safe = all(1 <= l[i] - l[i-1] <= 3 for i in range(1, len(l)))
+    elif sign == -1:
+        safe = all(1 <= l[i-1] - l[i] <= 3 for i in range(1, len(l)))
+    safe_lvls += safe
+
+# print(safe_lvls)
+    
+# PART 2
